35c9a549c43442a5708bc9235aadf108a6cf7522
SARATOV\Bakhytgul_Akhmetova <bakhytgulal4@gmail.com>
2018-10-23 23:20:13 +0400

    task 4 select fixing

--- Task_4_List_Of_Patients_P2/client/src/pages/VisitPage/components/FormVisit/FormVisit.js
+++ Task_4_List_Of_Patients_P2/client/src/pages/VisitPage/components/FormVisit/FormVisit.js
@@ -11,46 +11,19 @@ import { visitStore } from '../../../../store';
 
 import './FormVisit.scss';
 
-const patientOption = { value: 'Id', label: '' };
-const doctorOption = { value: 'Id', label: '' };
-const descriptionOption = { value: 'Id', label: '' };
+const option = { value: 'Id', label: '' };
 
-const visit = {
-    patientId: -1,
-    doctorId: -1,
-    descriptionId: -1,
-    date: new Date()
-};
+// const visit = {
+//     patientId: -1,
+//     doctorId: -1,
+//     descriptionId: -1,
+//     date: new Date()
+// };
 
 const mapActionsToProps  = {
-    onChangePatient : ({ patient, changePatient }) => (inputValue) => {
-        changePatient({ ...patient, label: inputValue });
-    },
-    onChangeDoctor : ({ doctor, changeDoctor }) => (inputValue) => {
-        changeDoctor({ ...doctor, label: inputValue });
-    },
-    onChangeDescription : ({ description, changeDescription }) => (inputValue) => {
-        changeDescription({ ...description, label: inputValue });
-    },
-    onSelectPatient : ({ changePatient }) =>  (selected) => changePatient(selected),
-    // ({ patient, changePatient }) => (selected) => {
-    //     // changePatient({ ...patient, value: selected.value, label: selected.label });
-    //     // patientOption.value = patient.value;
-    //     // patientOption.label = patient.label;
-    //     // visit.patientId = patient.value;
-    // },
-    onSelectDoctor : ({ doctor, changeDoctor }) => (selected) => {
-        changeDoctor({ ...doctor, value: selected.value, label: selected.label });
-        doctorOption.value = doctor.value;
-        doctorOption.label = doctor.label;
-        visit.doctorId = doctor.value;
-    },
-    onSelectDescription : ({ description, changeDescription }) => (selected) => {
-        changeDescription({ ...description, value: selected.value, label: selected.label });
-        descriptionOption.value = description.value;
-        descriptionOption.label = description.label;
-        visit.descriptionId = description.value;
-    }
+    onChangePatient : ({ changePatient }) => (inputValue) => changePatient({ inputValue }),
+    onChangeDoctor : ({ changeDoctor }) => (inputValue) => changeDoctor({ inputValue }),
+    onChangeDescription : ({ changeDescription }) => (inputValue) =>  changeDescription({ inputValue })
 };
 
 const getPatientOptions = _.debounce(visitStore.getSelectedPatients, 1000);
@@ -58,16 +31,9 @@ const getDoctorOptions = _.debounce(visitStore.getSelectedDoctors, 1000);
 const getDescriptionOptions = _.debounce(visitStore.getSelectedDescriptions, 1000);
 
 const Form  = ({
-    patient,
-    doctor,
-    description,
     onChangePatient,
     onChangeDoctor,
-    onChangeDescription,
-    onSelectPatient,
-    onSelectDoctor,
-    onSelectDescription }) => {
-    console.log(patient);
+    onChangeDescription }) => {
     return (
         <form className='form'>
             <div className='form__field'>
@@ -78,9 +44,7 @@ const Form  = ({
                     <div className='field--visit'>
                         <AsyncSelect
                             cacheOptions
-                            onChange={onSelectPatient}
                             onInputChange={onChangePatient}
-                            value={patient}
                             loadOptions={getPatientOptions}
                             className='field--visit__select'/>
                     </div>
@@ -94,9 +58,7 @@ const Form  = ({
                     <div className='field--visit'>
                         <AsyncSelect
                             cacheOptions
-                            onChange={onSelectDoctor}
                             onInputChange={onChangeDoctor}
-                            value={doctor}
                             loadOptions={(input) => getDoctorOptions(input)}
                             className='field--visit__select'/>
                     </div>
@@ -124,9 +86,7 @@ const Form  = ({
                     <div className='field--visit'>
                         <AsyncSelect
                             cacheOptions
-                            onChange={onSelectDescription}
                             onInputChange={onChangeDescription}
-                            value={description}
                             loadOptions={(input) => getDescriptionOptions(input)}
                             className='field--visit__select'/>
                     </div>
@@ -137,15 +97,6 @@ const Form  = ({
 };
 
 Form.propTypes = {
-    patient: PropTypes.string,
-    doctor: PropTypes.string,
-    description: PropTypes.string,
-    patientsOptions: PropTypes.array,
-    doctorsOptions: PropTypes.array,
-    descriptionsOptions: PropTypes.array,
-    onSelectPatient: PropTypes.func,
-    onSelectDoctor: PropTypes.func,
-    onSelectDescription: PropTypes.func,
     onChangePatient: PropTypes.func,
     onChangeDoctor: PropTypes.func,
     onChangeDescription: PropTypes.func
@@ -153,10 +104,8 @@ Form.propTypes = {
 
 export const FormVisit = compose(
     observer,
-    withState('patient', 'changePatient', patientOption),
-    withState('doctor', 'changeDoctor', doctorOption),
-    withState('description', 'changeDescription', descriptionOption),
-    withProps(({ patient, doctor, description }) => {
-        return { patient, doctor, description };
-    }),
+    withProps(),
+    withState('patient', 'changePatient', option),
+    withState('doctor', 'changeDoctor', option),
+    withState('description', 'changeDescription', option),
     withHandlers(mapActionsToProps))(Form);
