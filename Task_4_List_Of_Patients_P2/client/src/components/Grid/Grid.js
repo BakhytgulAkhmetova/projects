ae21de5d65baaaf949caf6a703f23be83e70c1d9
SARATOV\Bakhytgul_Akhmetova <bakhytgulal4@gmail.com>
2018-10-31 17:46:17 +0400

    task 4 Grid component deleted observer

--- Task_4_List_Of_Patients_P2/client/src/components/Grid/Grid.js
+++ Task_4_List_Of_Patients_P2/client/src/components/Grid/Grid.js
@@ -1,62 +1,45 @@
 import React from 'react';
-import { observer } from 'mobx-react';
+import PropTypes from 'prop-types';
 
 import { Link } from 'react-router-dom';
 
 import './Grid.scss';
 
-export const Grid = observer(({ columns, listItems, handlerOpenModalEdit }) => {
-    return (
-        <table
-            cellSpacing='0'
-            className='table-info'>
-            <thead>
-                <tr className='table-info__row thead'>
-                    <td className='table-info__row__cell' />
-                    {
-                        columns.map((c) => {
-                            return (<td
-                                key={c.header}
-                                className='table-info__row__cell'>
-                                {c.header}
-                            </td>);
-                        })
-                    }
-                </tr>
-            </thead>
-            <tbody className='table-info-tbody'>
-                {
-                    listItems.map((item) => {
-                        return (
-                            <tr
-                                key={item.id}
-                                className='table-info__row'>
-                                <td className='table-info__row__cell'>
-                                    <Link
-                                        onClick={handlerOpenModalEdit}
-                                        id={item.id}
-                                        to='/'
-                                        className='table-info__row__cell__link'>
-                                        Edit
-                                    </Link>
-                                </td>
-                                {
-                                    columns.map((c) => {
-                                        return (
-                                            <td
-                                                key={c.field}
-                                                className={'table-info__row__cell'}>
-                                                {item[c.field]}
-                                            </td>
-                                        );
-                                    })
-                                }
-                            </tr>
-                        );
-                    })
-                }
-            </tbody>
-            <tfoot />
-        </table>
-    );
-});
+export const Grid = ({ columns, listItems, handlerOpenModalEdit }) => (
+    <table cellSpacing='0' className='table-info'>
+        <thead>
+            <tr className='table-info__row thead'>
+                <td className='table-info__row__cell' />
+                {columns.map((c) => {
+                    return (<td key={c.header} className='table-info__row__cell'>
+                        {c.header}
+                    </td>);
+                })}
+            </tr>
+        </thead>
+        <tbody className='table-info-tbody'>{listItems.map((item) => {
+            return (<tr key={item.id} className='table-info__row'>
+                <td className='table-info__row__cell'>
+                    <Link
+                        onClick={handlerOpenModalEdit}
+                        id={item.id} to='/'
+                        className='table-info__row__cell__link'>
+                    Edit
+                    </Link>
+                </td>
+                {columns.map((c) => {
+                    return (<td key={c.field} className={'table-info__row__cell'}>
+                        {item[c.field]}
+                    </td>);
+                })}
+            </tr>);
+        })}
+        </tbody>
+        <tfoot />
+    </table>);
+
+Grid.propTypes = {
+    columns: PropTypes.array,
+    listItems: PropTypes.array,
+    handlerOpenModalEdit: PropTypes.func
+};
